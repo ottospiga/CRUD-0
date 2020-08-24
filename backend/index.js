57cdc36ca9354f68f041a4ee5df4059223ceb7cf
oyyo120 <ottospi@hotmail.com>
2020-08-23 21:46:14 -0300

    front iniciado e back funcional para login

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://amora:amoraVoadora@cluster0.raig6.mongodb.net/<dbname>?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+app.use('/api',require('./routes/index.js'))
+
+const port = process.env.PORT | 80
+
+app.listen(port)
